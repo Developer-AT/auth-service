35efd22205246dcbb25155ebf2e2ab5b528090b3
Abhishek Tyagi <abhishektyagi199816@gmail.com>
2022-09-14 23:19:21 +0530

    style: describe JWT provider fn's

--- src/providers/jwt/jwt.provider.ts
+++ src/providers/jwt/jwt.provider.ts
@@ -19,11 +19,18 @@ export class JwtProvider {
     private verifyOptions: VerifyOptions;
     constructor(private configService: ConfigService) {}
 
+    /**
+     * @description Convert Data Object to string
+     * @param payload Data Need to Encrypt
+     */
     setPayload(payload: { [key: string]: any }) {
         this.payload = JSON.stringify(payload);
     }
 
-    setSignOptions() {
+    /**
+     * @description Set Token Signature related Detail
+     */
+    private setSignOptions() {
         this.signOptions = {
             algorithm: this.configService.get<Algorithm>('service.algo'),
             expiresIn:
@@ -32,28 +39,47 @@ export class JwtProvider {
         };
     }
 
-    setSecretKey() {
+    /**
+     * @description Read and Set private key of Auth Service
+     */
+    private setSecretKey() {
         this.privateKey = readFileSync(
             this.configService.get<string>('service.keys.private.auth'),
             'utf8',
         );
     }
 
+    /**
+     * @description Encrypt Payload Data
+     * @returns {string} Encrypted Data
+     */
     signPayload() {
         try {
+            this.setSignOptions();
+            this.setSecretKey();
             return sign(this.payload, this.privateKey, this.signOptions);
         } catch (error) {
             Promise.reject(error);
         }
     }
 
-    setVerifyOptions() {
+    /**
+     * @description Set Requirements To verify Encrypted Data
+     */
+    private setVerifyOptions() {
         this.verifyOptions = {
             algorithms: [this.configService.get<Algorithm>('service.algo')],
             ignoreExpiration: false,
+            complete: true,
         };
     }
 
+    /**
+     * @description Verify the JWT token by token origin(Service Type)
+     * @param {ServiceType} serviceType To identify the token origin
+     * @param {string} token JWT Token
+     * @returns {Jwt | JwtPayload} Decode Token
+     */
     verifyToken(serviceType: ServiceType, token: string) {
         try {
             this.setVerifyOptions();
@@ -64,7 +90,12 @@ export class JwtProvider {
         }
     }
 
-    fetchPublicKeyByServiceType(serviceType: ServiceType) {
+    /**
+     * @description Read public key of Service by its origin type
+     * @param {ServiceType} serviceType To identify the token origin
+     * @returns Public Key
+     */
+    private fetchPublicKeyByServiceType(serviceType: ServiceType) {
         let key;
         switch (serviceType) {
             case 'auth':
